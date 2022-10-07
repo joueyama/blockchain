5b5f63fb73783f06782fece2404ba05b1a85414e
Jó Ueyama <64229250+joueyama@users.noreply.github.com>
2022-10-07 18:49:49 -0300

    Update jocoin.sol

--- jocoin.sol
+++ jocoin.sol
@@ -1,4 +1,4 @@
-// Code based on Hadcoins ICO https://github.com/hagenderouen/mini-chain/blob/master/hadcoins_ico.sol
+// Code based on jocoins ICO https://github.com/hagenderouen/mini-chain/blob/master/hadcoins_ico.sol
 
 // Version of compiler
 pragma solidity ^0.5.0;
@@ -9,24 +9,24 @@ contract jocoin_ico {
     uint public max_jocoins = 1000000;
 
     // Imprimindo a taxa de conversão de USD para jocoins
-    uint public usd_to_hadcoins = 1000;
+    uint public usd_to_jocoins = 1000;
 
     // Imprimindo o número total de jocoins que foram comprados por investidores
-    uint public total_hadcoins_bought = 0;
+    uint public total_jocoins_bought = 0;
 
     // Mapeamento do endereço do investidor para seu patrimônio em jocoins para USD
-    mapping(address => uint) equity_hadcoins;
+    mapping(address => uint) equity_jocoins;
     mapping(address => uint) equity_usd;
 
     // Verificando se um investidor pode comprar jocoins
-    modifier can_buy_hadcoins(uint usd_invested) {
-        require (usd_invested * usd_to_hadcoins + total_hadcoins_bought <= max_hadcoins);
+    modifier can_buy_jocoins(uint usd_invested) {
+        require (usd_invested * usd_to_jocoins + total_jocoins_bought <= max_jocoins);
         _;
     }
 
     // Obtendo o patrimônio em jocoins de um investidor
-    function equity_in_hadcoins(address investor) external view returns (uint) {
-        return equity_hadcoins[investor];
+    function equity_in_jocoins(address investor) external view returns (uint) {
+        return equity_jocoins[investor];
     }
 
     // Obtendo o patrimônio em dólares de um investidor
@@ -35,18 +35,18 @@ contract jocoin_ico {
     }
 
     // Comprando jocoins
-    function buy_hadcoins(address investor, uint usd_invested) external
-    can_buy_hadcoins(usd_invested) {
-        uint hadcoins_bought = usd_invested * usd_to_hadcoins;
-        equity_hadcoins[investor] += hadcoins_bought;
-        equity_usd[investor] = equity_hadcoins[investor] / 1000;
-        total_hadcoins_bought += hadcoins_bought;
+    function buy_jocoins(address investor, uint usd_invested) external
+    can_buy_jocoins(usd_invested) {
+        uint jocoins_bought = usd_invested * usd_to_jocoins;
+        equity_jocoins[investor] += jocoins_bought;
+        equity_usd[investor] = equity_jocoins[investor] / 1000;
+        total_jocoins_bought += jocoins_bought;
     }
 
     // Vendendo jocoins
-    function sell_hadcoins(address investor, uint hadcoins_sold) external {
-        equity_hadcoins[investor] -= hadcoins_sold;
-        equity_usd[investor] = equity_hadcoins[investor] / 1000;
-        total_hadcoins_bought -= hadcoins_sold;
+    function sell_jocoins(address investor, uint jocoins_sold) external {
+        equity_jocoins[investor] -= jocoins_sold;
+        equity_usd[investor] = equity_jocoins[investor] / 1000;
+        total_jocoins_bought -= jocoins_sold;
     }
 }
